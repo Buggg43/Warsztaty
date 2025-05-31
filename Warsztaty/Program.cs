2621ce4b6f9508ee8fbd11bb02cb1c38c5e53201
Buggg43 <kacperwerra12@gmail.com>
2025-05-31 12:08:28 +0200

    ff

--- Warsztaty/Program.cs
+++ Warsztaty/Program.cs
@@ -9,7 +9,7 @@ namespace Warsztaty
     {
         static void Main(string[] args)
         {
-            // 1. Pobranie globalnej instancji brokera (Singleton z biblioteki One1)
+            // 1. Pobranie globalnej instancji brokera (Singleton z biblioteki Contracts)
             IMessageBroker<string> broker = MessageBroker<string>.Instance;
 
             // 2. Utworzenie modułów, wstrzykujemy tę samą instancję brokera
@@ -23,7 +23,7 @@ namespace Warsztaty
 
             System.Threading.Thread.Sleep(500);
 
-            // 5. Zatrzymanie modułów
+            // 4. Zatrzymanie modułów
             Console.WriteLine("== Stop modułów ===");
             moduleOne.Stop();
             moduleTwo.Stop();
